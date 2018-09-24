77f13437fdf77caf703102f111c13deab763835b
Jaakko <jaakko@kronogra.fi>
2018-09-24 14:25:48 +0300

    shooting
    Changed the cursor image and started to make shooting mechanics

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -10,6 +10,16 @@ public class GunController : MonoBehaviour {
 	}
 	
 	void Update () {
+        RotateGun();
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    void RotateGun()
+    {
         Vector2 target = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
         // Get Angle in Radians
         float AngleRad = Mathf.Atan2(target.y - transform.position.y, target.x - transform.position.x);
@@ -17,5 +27,16 @@ public class GunController : MonoBehaviour {
         float AngleDeg = (180 / Mathf.PI) * AngleRad;
         // Rotate Object
         this.transform.rotation = Quaternion.Euler(0, 0, AngleDeg);
+    }
+    void Shoot()
+    {
+        int mask = 1 << 8;
+        //mask = ~mask;
+        Debug.Log("SHOT");
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up, Mathf.Infinity, mask);
+        if(hit.collider != null)
+        {
+            Debug.Log("HIT");
+        }
     }
 }
