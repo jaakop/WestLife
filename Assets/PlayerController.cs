9ada639f8bcdb19ad5bc30ba374c9ed46983380a
Jaakko <jaakko@kronogra.fi>
2018-10-07 12:09:41 +0300

    The change between right and left animation is now working porperly

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Animator playerAnim;
+
+	void Start () {
+		playerAnim = GetComponent<Animator>();
+	}
+
+	void Update () {
+        playerAnim.SetFloat("Direction", GetRotationToMouse());
+	}
+
+    float GetRotationToMouse()
+    {
+
+        Vector2 target = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        // Get Angle in Radians
+        float AngleRad = Mathf.Atan2(target.y - transform.position.y, target.x - transform.position.x);
+        // Get Angle in Degrees
+        float AngleDeg = (180 / Mathf.PI) * AngleRad;
+
+        return AngleDeg;
+    }
+
+}
